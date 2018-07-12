25453bc199481c538d21af96ec221245a61beeea
Richard <richardlam96@gmail.com>
2018-07-12 10:46:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.listen(3000, 'localhost', () => {
+	console.log('Discord Clone started.');
+});
