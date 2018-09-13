3f1c82c0eddc22f9bf94c30fd460083f5f59bb87
Richard <richardlam96@gmail.com>
2018-09-13 15:34:01 -0400

    removed production related code because of deployment errors

--- index.js
+++ index.js
@@ -102,10 +102,10 @@ io
 });
 
 // Production.
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 
